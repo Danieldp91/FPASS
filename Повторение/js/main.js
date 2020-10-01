958b824b3ca4900de905fafc4df10524c55feeab
Daniil Z <dpcomercial91@gmail.com>
2020-10-01 11:34:49 +0300

    Выполненое Практическое задание №3

---
+++ Повторение/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let name = 'Петя'
+
+function nameReading(Андрей) {
+    name = "Вася " + Андрей;
+    return;
+};
+nameReading('Дурак');
+console.log(name);
+
+/* 
+let car = "bmw";
+console.log('Привет ' + car); */
+]
